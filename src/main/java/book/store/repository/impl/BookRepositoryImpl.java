dcab5e06ec1be8bb490b45e687dd3ce005c7c046
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-02 08:03:27 +0200

    added main files

---
+++ src/main/java/book/store/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,47 @@
+package book.store.repository.impl;
+
+import book.store.model.Book;
+import book.store.repository.BookRepository;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't store the book to DB", e);
+        } finally {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            session.close();
+        }
+        return book;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("from Book", Book.class).getResultList();
+        }
+    }
+}
