cef84b57fb7096469613a578836571ea6b4b79bc
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-03 19:34:01 +0200

    modified BookMapper

--- src/main/java/book/store/mapper/BookMapper.java
+++ src/main/java/book/store/mapper/BookMapper.java
@@ -4,11 +4,14 @@ import book.store.config.MapperConfig;
 import book.store.dto.BookDto;
 import book.store.dto.CreateBookRequestDto;
 import book.store.model.Book;
+import java.util.List;
 import org.mapstruct.Mapper;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
     BookDto toDto(Book book);
+
+    List<BookDto> toDtoList(List<Book> bookList);
 
     Book toModel(CreateBookRequestDto requestDto);
 }
