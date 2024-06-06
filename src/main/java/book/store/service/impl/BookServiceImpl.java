23abf8064989c2de928e732a755b216465308f1a
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-06 16:50:09 +0200

    implemented new search method logic

--- src/main/java/book/store/service/impl/BookServiceImpl.java
+++ src/main/java/book/store/service/impl/BookServiceImpl.java
@@ -1,14 +1,17 @@
 package book.store.service.impl;
 
 import book.store.dto.BookDto;
+import book.store.dto.BookSearchParametersDto;
 import book.store.dto.CreateBookRequestDto;
 import book.store.exception.EntityNotFoundException;
 import book.store.mapper.BookMapper;
 import book.store.model.Book;
-import book.store.repository.BookRepository;
+import book.store.repository.book.BookRepository;
+import book.store.repository.book.BookSpecificationBuilder;
 import book.store.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class BookServiceImpl implements BookService {
     private static final String BOOK_NOT_FOUND = "Book not found by id: ";
     private final BookRepository bookRepository;
     private final BookMapper bookMapper;
+    private final BookSpecificationBuilder bookSpecificationBuilder;
 
     @Override
     public BookDto save(CreateBookRequestDto requestDto) {
@@ -51,6 +55,12 @@ public class BookServiceImpl implements BookService {
         Book bookToDelete = getBook(id);
         bookToDelete.setDeleted(true);
         return bookMapper.toDto(bookRepository.save(bookToDelete));
+    }
+
+    @Override
+    public List<BookDto> search(BookSearchParametersDto params) {
+        Specification<Book> bookSpecification = bookSpecificationBuilder.build(params);
+        return bookMapper.toDtoList(bookRepository.findAll(bookSpecification));
     }
 
     private Book getBook(Long id) {
