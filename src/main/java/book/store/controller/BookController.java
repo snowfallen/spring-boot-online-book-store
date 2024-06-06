97fe64a5559f327472b8f998840a8cb4575cde7c
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-06 11:54:45 +0200

    reset to prev commit

--- src/main/java/book/store/controller/BookController.java
+++ src/main/java/book/store/controller/BookController.java
@@ -5,11 +5,9 @@ import book.store.dto.CreateBookRequestDto;
 import book.store.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,16 +30,6 @@ public class BookController {
 
     @GetMapping
     public List<BookDto> getAll() {
-        return bookService.getAll();
-    }
-
-    @PutMapping("/{id}")
-    public BookDto updateById(@PathVariable Long id, @RequestBody CreateBookRequestDto bookDto) {
-        return bookService.updateById(id, bookDto);
-    }
-
-    @DeleteMapping("/{id}")
-    public BookDto deleteById(@PathVariable Long id) {
-        return bookService.deleteById(id);
+        return bookService.findAll();
     }
 }
