a4846e561b4506e9c30b750542b005e58c59712d
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-07 14:38:10 +0200

    added validation for book DTO

--- src/main/java/book/store/dto/CreateBookRequestDto.java
+++ src/main/java/book/store/dto/CreateBookRequestDto.java
@@ -1,13 +1,24 @@
 package book.store.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
+    @Length(min = 2)
     private String title;
+    @NotNull
+    @Length(min = 2)
     private String author;
+    @NotNull
+    @Length(min = 13, max = 13)
     private String isbn;
+    @NotNull
+    @Min(1)
     private BigDecimal price;
     private String description;
     private String coverImage;
