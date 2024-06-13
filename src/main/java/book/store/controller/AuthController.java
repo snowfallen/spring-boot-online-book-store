291ac6166cf580a004f05746446c611d46bc1941
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-13 13:26:40 +0200

    added auth controller

---
+++ src/main/java/book/store/controller/AuthController.java
@@ -0,0 +1,25 @@
+package book.store.controller;
+
+import book.store.dto.user.UserRegistrationRequestDto;
+import book.store.dto.user.UserResponseDto;
+import book.store.exception.RegistrationException;
+import book.store.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto)
+            throws RegistrationException {
+        return userService.register(requestDto);
+    }
+}
