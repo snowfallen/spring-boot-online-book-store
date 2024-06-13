806f7ba24cf167c6c6fef9ee72a9a2de5106c7dc
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-13 16:13:13 +0200

    added existsByEmail method in UserRepository

--- src/main/java/book/store/repository/user/UserRepository.java
+++ src/main/java/book/store/repository/user/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserByEmail(String email);
+    boolean existsByEmail(String email);
 }
