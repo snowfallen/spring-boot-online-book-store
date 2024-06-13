d32ae0d4c805e116c822dfe65177b0f4f3c56be7
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-13 13:22:09 +0200

    added User model

---
+++ src/main/java/book/store/model/User.java
@@ -0,0 +1,25 @@
+package book.store.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, name = "first_name")
+    private String firstName;
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+    @Column(name = "shipping_address")
+    private String shippingAddress;
+}
