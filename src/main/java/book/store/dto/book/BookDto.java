daaf856ec30901041a96c14d99f81aa6a39109e3
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-13 13:27:57 +0200

    moved books dto to book dto-s directory

--- src/main/java/book/store/dto/book/BookDto.java
+++ src/main/java/book/store/dto/book/BookDto.java
@@ -1,9 +1,12 @@
-package book.store.dto;
+package book.store.dto.book;
 
 import java.math.BigDecimal;
-import lombok.Data;
 
-@Data
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class BookDto {
     private Long id;
     private String title;
