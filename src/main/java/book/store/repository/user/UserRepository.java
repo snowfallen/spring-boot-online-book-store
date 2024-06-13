242a6ed34c7b85d9fa6bd26d60202c5e446327a3
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-13 16:14:08 +0200

    fixed code-style

--- src/main/java/book/store/repository/user/UserRepository.java
+++ src/main/java/book/store/repository/user/UserRepository.java
@@ -1,7 +1,6 @@
 package book.store.repository.user;
 
 import book.store.model.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
