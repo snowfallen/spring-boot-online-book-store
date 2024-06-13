99f23cb7b08bb117af870be8e0fad903b202f971
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-13 13:44:37 +0200

    added annotation for swagger in AuthController

--- src/main/java/book/store/controller/AuthController.java
+++ src/main/java/book/store/controller/AuthController.java
@@ -4,6 +4,8 @@ import book.store.dto.user.UserRegistrationRequestDto;
 import book.store.dto.user.UserResponseDto;
 import book.store.exception.RegistrationException;
 import book.store.service.user.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Auth management", description = "Endpoints for authentication")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
 
+    @Operation(summary = "Create a new user", description = "Create a new user")
     @PostMapping("/register")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto)
             throws RegistrationException {
