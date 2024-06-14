c7277d6d2b36741f4a2363c34ed10edec59a98f5
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-14 18:10:16 +0200

    added findByEmail method into user repository

--- src/main/java/book/store/repository/user/UserRepository.java
+++ src/main/java/book/store/repository/user/UserRepository.java
@@ -1,10 +1,17 @@
 package book.store.repository.user;
 
 import book.store.model.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    @Query("FROM User u LEFT JOIN FETCH u.roles r "
+            + "WHERE u.email = :email "
+            + "AND r.isDeleted = FALSE")
+    Optional<User> findUserByEmail(String email);
 }
