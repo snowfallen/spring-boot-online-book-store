c35f8e2bad17198a87238aed296cef909becf1e7
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-14 18:09:25 +0200

    added Role repository

---
+++ src/main/java/book/store/repository/role/RoleRepository.java
@@ -0,0 +1,11 @@
+package book.store.repository.role;
+
+import book.store.model.Role;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findRoleByName(Role.RoleName roleName);
+}
