aebeec31e605a658c88d9863e555241cdd52db2e
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-15 19:56:11 +0200

    created dto for user login request

---
+++ src/main/java/book/store/dto/user/UserLoginRequestDto.java
@@ -0,0 +1,16 @@
+package book.store.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserLoginRequestDto(
+        @Email
+        @NotBlank
+        @Size(min = 8, max = 255)
+        String email,
+        @NotBlank
+        @Size(min = 8, max = 255)
+        String password
+) {
+}
