678da03111ace59ae0ce83068b8c0fa37f5ad22a
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-19 10:48:33 +0200

    added new method to book mapper

--- src/main/java/book/store/mapper/BookMapper.java
+++ src/main/java/book/store/mapper/BookMapper.java
@@ -2,17 +2,45 @@ package book.store.mapper;
 
 import book.store.config.MapperConfig;
 import book.store.dto.book.BookDto;
+import book.store.dto.book.BookDtoWithoutCategoryIds;
 import book.store.dto.book.CreateBookRequestDto;
 import book.store.model.Book;
+import book.store.model.Category;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.springframework.data.domain.Page;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
+    @Mapping(target = "categoryIds", ignore = true)
     BookDto toDto(Book book);
+
+    @AfterMapping
+    default void setCategoryIds(@MappingTarget BookDto bookDto, Book book) {
+        bookDto.setCategoryIds(book.getCategories().stream()
+                .map(Category::getId)
+                .collect(Collectors.toSet()));
+    }
+
+    List<BookDtoWithoutCategoryIds> toBookDtoWithoutCategoryIdsList(List<Book> book);
 
     List<BookDto> toDtoList(Page<Book> bookList);
 
+    @Mapping(target = "categories", ignore = true)
     Book toModel(CreateBookRequestDto requestDto);
+
+    @AfterMapping
+    default void setCategories(@MappingTarget Book book, CreateBookRequestDto requestDto) {
+        Set<Category> categories = requestDto.getCategoryIds()
+                .stream()
+                .map(Category::new)
+                .collect(Collectors.toSet());
+        book.setCategories(categories);
+    }
 }
+
