dda4963890bd09bca4e238f932fed98d41748d7e
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-19 10:46:07 +0200

    implemented category service interface

---
+++ src/main/java/book/store/service/category/impl/CategoryServiceImpl.java
@@ -0,0 +1,58 @@
+package book.store.service.category.impl;
+
+import book.store.dto.category.CategoryDto;
+import book.store.dto.category.CreateCategoryRequestDto;
+import book.store.exception.EntityNotFoundException;
+import book.store.mapper.CategoryMapper;
+import book.store.model.Category;
+import book.store.repository.category.CategoryRepository;
+import book.store.service.category.CategoryService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    public static final String CATEGORY_NOT_FOUND_BY_ID = "Category not found by id: ";
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+    @Override
+    public List<CategoryDto> getAll(Pageable pageable) {
+        return categoryMapper.toDtoList(categoryRepository.findAll(pageable));
+    }
+
+    @Override
+    public CategoryDto getById(Long id) {
+        return categoryMapper.toDto(getCategory(id));
+    }
+
+    @Override
+    public CategoryDto save(CreateCategoryRequestDto categoryRequestDto) {
+        Category category = categoryMapper.toModel(categoryRequestDto);
+        return categoryMapper.toDto(categoryRepository.save(category));
+    }
+
+    @Override
+    public CategoryDto update(Long id, CreateCategoryRequestDto categoryRequestDto) {
+        Category category = getCategory(id);
+        category.setName(categoryRequestDto.getName());
+        category.setDescription(categoryRequestDto.getDescription());
+        return categoryMapper.toDto(categoryRepository.save(category));
+    }
+
+    @Override
+    public CategoryDto deleteById(Long id) {
+        Category category = getCategory(id);
+        category.setDeleted(true);
+        return categoryMapper.toDto(categoryRepository.save(category));
+    }
+
+    private Category getCategory(Long id) {
+        return categoryRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(CATEGORY_NOT_FOUND_BY_ID + id)
+        );
+    }
+}
