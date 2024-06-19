c4dd94a5a3d8ad0df2a6eac8936e7e2ad832f50a
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-19 10:49:44 +0200

    refactored create book dto

--- src/main/java/book/store/dto/book/CreateBookRequestDto.java
+++ src/main/java/book/store/dto/book/CreateBookRequestDto.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
+import java.util.Set;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Length;
 
@@ -23,4 +24,6 @@ public class CreateBookRequestDto {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    @NotNull
+    private Set<Long> categoryIds;
 }
