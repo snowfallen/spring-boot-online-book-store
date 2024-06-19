a0247638a3744e7da31044f025e114903da87846
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-06-19 10:48:11 +0200

    refactored book dto

--- src/main/java/book/store/dto/book/BookDto.java
+++ src/main/java/book/store/dto/book/BookDto.java
@@ -1,6 +1,7 @@
 package book.store.dto.book;
 
 import java.math.BigDecimal;
+import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,4 +15,5 @@ public class BookDto {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    private Set<Long> categoryIds;
 }
