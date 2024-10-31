3be4fc3d3375fbf7c28ae11894f36be5452951dd
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-10-31 12:13:44 +0100

    add cart item model

---
+++ src/main/java/book/store/model/CartItem.java
@@ -0,0 +1,31 @@
+package book.store.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(name = "cart_items")
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "shopping_cart_id", nullable = false)
+    private ShoppingCart shoppingCart;
+    @OneToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+}
