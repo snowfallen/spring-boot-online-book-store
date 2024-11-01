4a50c30ffeef441c64fc4fba85a75bce27f31b4a
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-11-01 16:05:32 +0100

    refactor mappers

--- src/main/java/book/store/mapper/CartItemMapper.java
+++ src/main/java/book/store/mapper/CartItemMapper.java
@@ -3,12 +3,18 @@ package book.store.mapper;
 import book.store.config.MapperConfig;
 import book.store.dto.cart.item.CartItemResponseDto;
 import book.store.model.CartItem;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
 @Mapper(config = MapperConfig.class)
 public interface CartItemMapper {
+    @Mapping(target = "bookId", ignore = true)
+    @Mapping(target = "bookTitle", ignore = true)
     CartItemResponseDto toDto(CartItem cartItem);
 
     @AfterMapping
@@ -21,5 +27,12 @@ public interface CartItemMapper {
     default void setBookTitle(@MappingTarget CartItemResponseDto cartItemResponseDto,
                               CartItem cartItem) {
         cartItemResponseDto.setBookTitle(String.valueOf(cartItem.getBook().getTitle()));
+    }
+
+    @Named("toDtoSet")
+    default Set<CartItemResponseDto> toDtoSet(Set<CartItem> cartItems) {
+        return cartItems.stream()
+                .map(this::toDto)
+                .collect(Collectors.toSet());
     }
 }
