f77f81112fb7f8958e8449f01334b6d4fa4e0f81
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-11-01 16:04:57 +0100

    refactor dto-s

--- src/main/java/book/store/dto/cart/item/CartItemUpdateQuantityDto.java
+++ src/main/java/book/store/dto/cart/item/CartItemUpdateQuantityDto.java
@@ -1,9 +1,11 @@
 package book.store.dto.cart.item;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record CartItemUpdateQuantityDto(
         @Positive
+        @NotNull
         int quantity
 ) {
 }
