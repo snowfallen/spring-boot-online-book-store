0436bb25caabdf6d712ba45ccccb5406f2abff33
snowfallen <bondarenkoov.dev@gmail.com>
2025-03-27 14:41:31 +0100

    created dto-s

---
+++ src/main/java/book/store/dto/order/OrderItemResponseDto.java
@@ -0,0 +1,12 @@
+package book.store.dto.order;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderItemResponseDto {
+    private Long id;
+    private Long bookId;
+    private int quantity;
+} 
